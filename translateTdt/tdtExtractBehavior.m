bb893be5ce1fd84ff4ee42b42bdfdc45fe5b3530
Chenchal Subraveti <chenchals@gmail.com>
2018-07-24 17:45:57 -0500

    added additional columns for verification

--- translateTdt/tdtExtractBehavior.m
+++ translateTdt/tdtExtractBehavior.m
@@ -1,4 +1,4 @@
-function [oFiles, trialEventTimesTbl, trialUnknownEventTimesTbl, trialCodes, trialTimes, evCodec, infosCodec, tdtEvents,tdtEventTimes, tdtInfos ] = tdtExtractBehavior(sessionDir, outputBaseDir, eventCodecFile, infosCodecFile)
+function [trialsTbl, evCodec, infosCodec, tdtInfos ] = tdtExtractBehavior(sessionDir, outputBaseDir, eventCodecFile, infosCodecFile)
 %TDTEXTRACTBEHAVIOR Summary of this function goes here
 %   Detailed explanation goes here
 %
@@ -37,7 +37,7 @@ function [oFiles, trialEventTimesTbl, trialUnknownEventTimesTbl, trialCodes, tri
 %    [oFiles] = tdtExtractBehavior(sessDir, outDir, evDefFile, infosDefFile);
 
     % Initialize output files
-    oFiles = {};
+    %oFiles = {};
 
     useTaskEndCode = false;
     
@@ -100,13 +100,19 @@ function [oFiles, trialEventTimesTbl, trialUnknownEventTimesTbl, trialCodes, tri
     nTasks = size(evCodes,1);
     colNames = evCodec.name2Code.keys';
     colCodes = cell2mat(evCodec.name2Code.values'); 
-    colNames = [colNames;'TaskType_';'GoodTrial';'HasInfosCodes'];
+    colNames = [colNames;'TaskType_';'GoodTrial';'HasInfosCodes';'HasTrialStartAndEot';'HasStartInfosAndEndInfos'];
     %trialsTbl = cell2table(cell(0,numel(colNames)));
     trialsTbl = array2table(nan(nTasks,numel(colNames)));
     trialsTbl.DuplicateEventCodes = cell(nTasks,1);
     trialsTbl.Properties.VariableNames(1:end-1) = colNames;
     warning('OFF','MATLAB:table:RowsAddedExistingVars');
     ignoreDuplicateEvents = [2777 2776];% manual juice...
+    % codes
+    trialStartCode = evCodec.name2Code('TrialStart_');
+    trialEndCode = evCodec.name2Code('Eot_');
+    infosStartCode = evCodec.name2Code('StartInfos_');
+    infosEndCode = evCodec.name2Code('EndInfos_');
+
 tic
     for t = 1:nTasks
         allC = evCodes{t};
@@ -119,8 +125,12 @@ tic
         dups = evCodesTemp(setdiff(1:numel(evCodesTemp),iUniq));
         tms = tmsTemp(iUniq);
         iInfos = allC(allC >= 3000);
+        % default some vars to be present
         trialsTbl.GoodTrial(t) = 1;
-        trialsTbl.HasInfoCodes(t) = 1;
+        trialsTbl.HasInfosCodes(t) = 1;
+        trialsTbl.HasTrialStartAndEot(t) = ismember(trialStartCode, evs) && ismember(trialEndCode, evs);
+        trialsTbl.HasStartInfosAndEndInfos(t) = ismember(infosStartCode, evs) && ismember(infosEndCode, evs);
+        
         if numel(evs) ~= numel(ilt3000)
             warning('Task block %d has duplicate event codes\n',t);
             trialsTbl.DuplicateEventCodes(t) = {unique(dups)'}; 
@@ -142,99 +152,11 @@ tic
     end
    toc 
     
-    
-    % Add an extra Event at the end to help in arryfun below
-    iTrialStartTemp = [find(tdtEvents==trialStartCode);nEvents+1];% all TrialStart_
-    iEotTemp = [find(tdtEvents==eotCode);nEvents+1];% all Eot_
-    iEndInfosTemp = [find(tdtEvents==endInfosCode);nEvents+1];% all endInfos
-    
-    % find iTrialStart,iEot,iEndInfos between currTask and nextTask
-    % It is possible that more than 1 of the same eventCXode is sent
-    % successively, hence take the minimum of such index. If empty, min()
-    % fx inside call to arrayfun() will use the nEvents+1 thast is setup
-    % above as the index into the tdtEvents, hence lines below change the
-    % nEvents+1 to NaN.  
-    iTrialStart = iTrialStartTemp(arrayfun(@(i) min([...
-        find(iTrialStartTemp>iTaskStart(i) & iTrialStartTemp<iTaskEnd(i),1);...
-        numel(iTrialStartTemp)]),...% extra index here
-        (1:length(iTaskStart))'));
-    
-    iEot = iEotTemp(arrayfun(@(i) min([...
-        find(iEotTemp>iTaskStart(i) & iEotTemp<iTaskEnd(i),1);...
-        numel(iEotTemp)]),...% extra index here
-        (1:length(iTaskStart))'));
-    
-    iEndInfos = iEndInfosTemp(arrayfun(@(i) min([...
-        find(iEndInfosTemp>iTaskStart(i) & iEndInfosTemp<iTaskEnd(i),1);...
-        numel(iEndInfosTemp)]),...
-        (1:length(iTaskStart))'));    
-    % Set the extra index to NaN
-    iTrialStart(iTrialStart>nEvents) = NaN;
-    iEot(iEot>nEvents) = NaN;
-    iEndInfos(iEndInfos>nEvents) = NaN;
-    % augment thes above indices (into tdtEvs/tdtEvTms).
-    % Rows of augment, evCodes evTimes correspond to putative trial numbers
-    % 
-    augmented = [iTrialStart iEot iEndInfos iTaskStart iTaskEnd];
-    % Find indices row indicces of augmented above where iTrialStart, iEot,
-    % iEndInfos are NOT NaN for the Row aka complete cases.
-    % NOTE: Here we are using only  trials where iEndInfos is NOT NAN 
-    % NOTE: Could have used al three indices above...
-    if ~useTaskEndCode
-        trialsWithEndInfos = find(sum(isnan(augmented(:,3)),2)==0);
-        validTrials = trialsWithEndInfos;
-    else        
-        trialsWithTaskStartAndTaskEnd = find(sum(isnan(augmented(:,4:5)),2)==0);
-        validTrials = trialsWithTaskStartAndTaskEnd;
-    end
-    % prune evCodes and evTimes to valid task chunks
-    trialCodes = evCodes(validTrials);
-    trialTimes = evTimes(validTrials);
-    
-    save('myEvCodesEvTimes.mat','trialCodes','trialTimes','evCodec','infosCodec');
-    
-    %% Convert cell array of trialCodes and trialTimes to trialEventTimesTbl %%
-    defTaskCodes = [1501:1510];
-    nTrials = numel(trialCodes);
-    names = evCodec.name2Code.keys';
-    names = ['TaskType_';names];
-    trialEventTimesTbl = array2table(nan(nTrials,numel(names)));
-    trialEventTimesTbl.Properties.VariableNames = names;
-    tic
-    % Keep track of unknown codes
-    unknownCodes = [];
-    unknownCodeCount = 0;
-    trialUnknownEventTimesTbl = table();% dynamically add columns when unknown code is encountered
-    for trlNo = 1:nTrials
-        tCodes = unique(trialCodes{trlNo},'stable'); tCodes(tCodes>=3000)=[];
-        for jj = 1:numel(tCodes)
-            code = tCodes(jj);
-            codeTime = trialTimes{trlNo}(find(trialCodes{trlNo} == code,1,'first'));
-            try
-                trialEventTimesTbl.(evCodec.code2Name(code))(trlNo) = codeTime;
-                if find(defTaskCodes==code)
-                    trialEventTimesTbl.TaskType_(trlNo) = code;
-                end
-            catch me
-                %warning('*** Trial# %d --> unknown code %d',trlNo,code);
-                unknownCodeCount = unknownCodeCount + 1;
-                unknownCodes(unknownCodeCount) = code;
-                dynCol = num2str(1,'code_%d');
-                if ~any(contains(trialUnknownEventTimesTbl.Properties.VariableNames,dynCol))
-                    trialUnknownEventTimesTbl.(dynCol)(trlNo) = codeTime;
-                end
-                
-            end
-        end
-    end
-    
-    toc
-    
     % Prune columns where values for all rows is NaN
     % Get only columns with at least one non NaN value in the colum
     % 
      %trialEventTimesTbl = trialEventTimesTbl(:,any(~ismissing(trialEventTimesTbl)));
-     trialEventTimesTbl(:,all(ismissing(trialEventTimesTbl)))=[];
+%     trialEventTimesTbl(:,all(ismissing(trialEventTimesTbl)))=[];
 %     T2 = trialEventTimesTbl;
 %     T2(:,all(ismissing(T2)))=[];
     
