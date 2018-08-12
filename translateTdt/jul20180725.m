5f54073dfaced3dbfa716d25537e0e106b29e920
stevenerrington <steven.errington@outlook.com>
2018-08-12 14:38:15 -0500

    SE github test: minor changes to output file name (now SEF_Cmand_*****).

--- translateTdt/jul20180725.m
+++ translateTdt/jul20180725.m
@@ -1,15 +1,14 @@
 
 % Base dir for data
 %baseDir = '/Volumes/schalllab/data/Joule/tdtData/troubleshootEventCodes';
-baseDir = 'data/Joule/tdtData/troubleshootEventCodes';
+baseDir = '/Volumes/SchallLab/data/Joule/tdtData/troubleshootEventCodes';
 %saveDir = fullfile(baseDir,'processed');
-saveDir = 'dataProcessed/data/Joule/tdtData/troubleshootEventCodes';
-eventDefFile = 'data/Joule/TEMPO/currProcLib_15/EVENTDEF.pro';
-infosDefFile = 'data/Joule/TEMPO/currProcLib_15/CMD/INFOS.pro';
-
+saveDir = '/Volumes/SchallLab/data/Joule/tdtData/troubleshootEventCodes/processed';
+eventDefFile = '/Volumes/SchallLab/data/Joule/TEMPO/currProcLib_24/EVENTDEF.pro';
+infosDefFile = '/Volumes/SchallLab/data/Joule/TEMPO/currProcLib_24/CMD/INFOS.pro';
 
 %Dirs
-jDatedSessions=dir(fullfile(baseDir,'Joule-180806-1344*'));
+jDatedSessions=dir(fullfile(baseDir,'Joule-180809-1546*'));
 
 saveOutput = 1;
 
@@ -36,9 +35,10 @@ for ii = 1:numel(blockPaths)
         if ~exist(oDir,'dir')
             mkdir(oDir);
         end
-        fprintf('Saving to file %s\n',fullfile(oDir,'Behav.mat'));
-        save(fullfile(oDir,'Behav.mat'),'Task');
-        save(fullfile(oDir,'Behav.mat'),'-append','taskInfos');
+        outputFilename = ['SEF_Cmand_' jDatedSessions.name '.mat'];
+        fprintf('Saving to file %s\n',fullfile(oDir,outputFilename));
+        save(fullfile(oDir,outputFilename),'Task');
+        save(fullfile(oDir,outputFilename),'-append','taskInfos');
     end
     outTrials.(regexprep(sessionName,'-','_')) = eventTable;
     outInfos.(regexprep(sessionName,'-','_')) = taskInfos;
