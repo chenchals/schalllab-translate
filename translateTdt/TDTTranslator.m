a440bba4a2417502d7b61b0b185c188d4d55e335
Chenchal Subraveti <chenchals@gmail.com>
2018-10-01 23:07:44 -0500

    Updated to use TDTTranslator class

--- translateTdt/TDTTranslator.m
+++ translateTdt/TDTTranslator.m
@@ -2,25 +2,27 @@ classdef TDTTranslator < matlab.mixin.SetGetExactNames
     %TDTTranslator Summary of this class goes here
     %   Detailed explanation goes here
     
-    properties (Access = protected)
+    properties (Access = private)
         options
-        spaces = {repmat(' ',1,5), repmat(' ',1,25)};
+        
+        
+        
         optionFieldPrompts = {
-            ['sessionDir:', sprintf('Location of TDT session directory\n\t\t\t\t[string]')]
-            ['baseSaveDir:', sprintf('Base directory for saving translation results\n\t(will create dirctoty with session_name)\n\t\t\t\t[string]')]
-            ['eventDefFile:', sprintf('Full filepath to location of EVENTDEF.pro file used for session\n\t\t\t\t[string]')]
-            ['infosDefFile:', sprintf('Full filepath to location of INFOS.pro file used for session\n\t\t\t\t [string]')]
-            ['hasEdfDataFile:', sprintf('Does session directory above contain \''dataEDF.mat\'' file?\n\t(This file is data collected on EYELINK computer and translated to \''dataEDF.mat\'' by third-party utility)\n\t\t\t\t[true|false]')]
+            {'sessionDir', sprintf('Location of TDT session directory\n\t\t\t\t[string]')}
+            {'baseSaveDir', sprintf('Base directory for saving translation results\n\t(will create dirctoty with session_name)\n\t\t\t\t[string]')}
+            {'eventDefFile', sprintf('Full filepath to location of EVENTDEF.pro file used for session\n\t\t\t\t[string]')}
+            {'infosDefFile', sprintf('Full filepath to location of INFOS.pro file used for session\n\t\t\t\t [string]')}
+            {'hasEdfDataFile', sprintf('Does session directory above contain \''dataEDF.mat\'' file?\n\t(This file is data collected on EYELINK computer and translated to \''dataEDF.mat\'' by third-party utility)\n\t\t\t\t[true|false]')}
             };
         edfOptionFieldPrompts = {
-            ['useEye:', sprintf('Which component of Eye data for TDT and EDF do you want to use for aligning? \n\t\t\t\t [char X|Y]')]
+            {'useEye', sprintf('Which component of Eye data for TDT and EDF do you want to use for aligning? \n\t\t\t\t [char X|Y]')}
             % ADC volt range of TDT
-            ['voltRange:', sprintf('What is the voltage range of Eyelink data sent to TDT?\n\tTypically the values are [-5 5]\n\t\t\t\t[2 element vector]')]
+            {'voltRange', sprintf('What is the voltage range of Eyelink data sent to TDT?\n\tTypically the values are [-5 5]\n\t\t\t\t[2 element vector]')}
             % Signal range of EDF typically [-0.2 1.2]?
-            ['signalRange:', sprintf('What is the signal range of Eyelink data sent to TDT?\n\tTypically the values are [-0.5 1.2]\n\t\t\t\t[2 element vector]')]
+            {'signalRange', sprintf('What is the signal range of Eyelink data sent to TDT?\n\tTypically the values are [-0.5 1.2]\n\t\t\t\t[2 element vector]')}
             % Screen pixel range for EDF eye movement
             %     Screen dimensions: X:[0 1024] or Y: [0 768]%
-            ['pixelRange:', sprintf('What is the pixel range (Screen dimension in Pixels) of Eyelink data sent to TDT?\n\tTypically the values are [0 1024] for X or [0 768] for Y\n\t\t\t\t[2 element vector]')]          
+            {'pixelRange', sprintf('What is the pixel range (Screen dimension in Pixels) of Eyelink data sent to TDT?\n\tTypically the values are [0 1024] for X or [0 768] for Y\n\t\t\t\t[2 element vector]')}
             };
 
     end
@@ -29,10 +31,16 @@ classdef TDTTranslator < matlab.mixin.SetGetExactNames
     end
     
     methods
-        function obj = TDTTranslator(inputArg1,inputArg2)
-            %TDT Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+        function obj = TDTTranslator(varargin)
+            %TDTTRANSLATOR Construct an instance of this class
+            if nargin==1
+                obj.options = varargin{1};
+                %checkOptions(obj);
+                error('*****Passing options - Not yet Implemented*****');
+            else
+                setOptions(obj);           
+            end
+            %translate(obj);
         end
         
         function setOptions(obj)
@@ -40,54 +48,55 @@ classdef TDTTranslator < matlab.mixin.SetGetExactNames
                 obj.options = struct();
             end
             obj.options = processFields(obj,obj.optionFieldPrompts);
-            verifyFileOptions(obj,obj.options);
+            %verifyFileOptions(obj,obj.options);
             if obj.options.hasEdfDataFile
                 obj.options.edf = processFields(obj,obj.edfOptionFieldPrompts);               
+                %verifyEdfOptions(obj,obj.options.edf);            
             end
-            verifyEdfOptions(obj,obj.options.edf);
-            
         end
         
-        function outputArg = translate(obj)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
+        function [Task, TaskInfos, EventCodec, InfosCodec, TrialEyes, SessionInfo] = translate(obj)
+            %TRANSLATE Summary of this method goes here
             checkOptions(obj);
-            outputArg = 'Ready to translate......!!!!'
+            o = obj.options;
+            [Task, TaskInfos, EventCodec, InfosCodec, TrialEyes, SessionInfo] = runExtraction(o.sessionDir, o.baseSaveDir, o.eventDefFile, o.infosDefFile, o.edf);
         end
     end
     methods (Access = private)
         function checkOptions(obj)
-            if isempty(obj.options)
+            if isempty(obj.options) || ~isstruct(obj.options)
                 warning('Processing options are not set');
                 warning('Setup options for processing ');
                 obj.setOptions();
-            end
+            end          
+            verifyFileOptions(obj,obj.options);
+            verifyEdfOptions(obj,obj.options.edf);
+           
         end
         
         function out = processFields(~,fieldnamePrompts)
             out = struct();
             for f = fieldnamePrompts'
-                of = split(f{1},':');
-                out.(of{1}) = input([of{2} ' = ']);
+                out.(f{1}{1}) = input([f{1}{2} ' = ']);
             end                       
         end
         
-        function verifyFileOptions(~,optStruct)
+        function verifyFileOptions(obj,optStruct)
             try
                 if ~exist(optStruct.sessionDir,'dir')
-                    throw(MException('TDTTranslator:DirectoryNotFound','sessionDir [%s] does not exist!',optStruct.sessionDir));
+                    throw(MException('TDTTranslator:DirectoryNotFound','options.sessionDir [%s] does not exist!',optStruct.sessionDir));
                 end
                 if ~exist(optStruct.baseSaveDir,'dir')
-                    throw(MException('TDTTranslator:DirectoryNotFound','baseSaveDir [%s] does not exist!',optStruct.baseSaveDir));
+                    throw(MException('TDTTranslator:DirectoryNotFound','options.baseSaveDir [%s] does not exist!',optStruct.baseSaveDir));
                 end
                 if ~exist(optStruct.eventDefFile,'file')
-                    throw(MException('TDTTranslator:FileNotFound','eventDefFile [%s] does not exist!',optStruct.eventDefFile));
+                    throw(MException('TDTTranslator:FileNotFound','options.eventDefFile [%s] does not exist!',optStruct.eventDefFile));
                 end
                 if ~exist(optStruct.infosDefFile,'file')
-                    throw(MException('TDTTranslator:FileNotFound','infosDefFile [%s] does not exist!',optStruct.infosDefFile));
+                    throw(MException('TDTTranslator:FileNotFound','options.infosDefFile [%s] does not exist!',optStruct.infosDefFile));
                 end
                 if optStruct.hasEdfDataFile && ~exist(fullfile(optStruct.sessionDir,'dataEDF.mat'),'file')
-                    throw(MException('TDTTranslator:FileNotFound','hasEdfDataFile is set to true, but file [%s] does not exist!',fullfile(optStruct.sessionDir,'dataEDF.mat')));
+                    throw(MException('TDTTranslator:FileNotFound','options.hasEdfDataFile is set to true, but file [%s] does not exist!',fullfile(optStruct.sessionDir,'dataEDF.mat')));
                 end
             catch me
                 error(me.message)
@@ -99,25 +108,24 @@ classdef TDTTranslator < matlab.mixin.SetGetExactNames
             
             try
                 if isempty(regexp(optStruct.useEye,'(?<p>[XY])','names','ignorecase'))
-                    throw(MException('TDTTranslator:IncorrectValue','edf.useEye must be [X or Y] but was [%s]!',optStruct.useEye));
+                    throw(MException('TDTTranslator:IncorrectValue','options.edf.useEye must be [X or Y] but was [%s]!',optStruct.useEye));
                 end              
                 if iscell(optStruct.voltRange) || numel(optStruct.voltRange)~=2 || any(isnan(optStruct.voltRange)) ...
                         || any(isinf(optStruct.voltRange)) || range(optStruct.voltRange)<=0
-                    throw(MException('TDTTranslator:IncorrectValue','edf.voltRange must be a 2 element non-NaN non-Inf numeric vector!'));
+                    throw(MException('TDTTranslator:IncorrectValue','options.edf.voltRange must be a 2 element non-NaN non-Inf numeric vector!'));
                 end
                 if iscell(optStruct.signalRange) || numel(optStruct.signalRange)~=2 || any(isnan(optStruct.signalRange)) ...
                         || any(isinf(optStruct.signalRange)) || range(optStruct.signalRange)<=0
-                    throw(MException('TDTTranslator:IncorrectValue','edf.signalRange must be a 2 element non-NaN non-Inf numeric vector!'));
+                    throw(MException('TDTTranslator:IncorrectValue','options.edf.signalRange must be a 2 element non-NaN non-Inf numeric vector!'));
                 end
                 if iscell(optStruct.pixelRange) || numel(optStruct.pixelRange)~=2 || any(isnan(optStruct.pixelRange)) ...
                         || any(isinf(optStruct.pixelRange)) || range(optStruct.pixelRange)<=0
-                    throw(MException('TDTTranslator:IncorrectValue','edf.pixelRange must be a 2 element non-NaN non-Inf numeric vector!'));
+                    throw(MException('TDTTranslator:IncorrectValue','options.edf.pixelRange must be a 2 element non-NaN non-Inf numeric vector!'));
                 end
             catch me
                 error(me.message)
             end
         end
-        
         
     end
 end
--- translateTdt/tdtAlignEyeWithEdf.m
+++ translateTdt/tdtAlignEyeWithEdf.m
@@ -1,4 +1,4 @@
-function [alignStartIndex, alignedEdfVec] = tdtAlignEyeWithEdf(edfEyeVec, tdtEyeVec, edfSamplingFreqHz, tdtSamplingFreqHz, alignWindowSecs)
+function [alignStartIndex, alignedEdfVec] = tdtAlignEyeWithEdf(edfEyeVec, tdtEyeVec, edfSamplingFreqHz, tdtSamplingFreqHz, alignWindowSecs, edfOptions)
 %TDTALIGNEYEWITHEDF Align EDF eye data with TDT eye data
 %Note:
 % Replace values in EDF eye data that is missed/defaulted
@@ -9,6 +9,11 @@ function [alignStartIndex, alignedEdfVec] = tdtAlignEyeWithEdf(edfEyeVec, tdtEye
 %   edfSamplingFreqHz : Sampling frequency of EDF
 %   tdtSamplingFreqHz : Sampling frequency of TDT for Eye channels
 %   alignWindowSecs : In Seconds. Converted to number of edf bins to slide the data for computing alignment.
+%   edfOptions : A struct with the following fields
+%                    useEye : Which eye data to use X or Y [not used in this function]
+%                    voltRange : voltage range of ADC [-5 5]
+%                    signalRange : signal range of Eyelink [-0.2 1.2]
+%                    pixelRange : screen pixels in X or Y [0 1024]
 %
 %   OUTPUT:
 %     alignStartIndex : The index of edfEyeVec that when aligned with the
@@ -37,25 +42,16 @@ function [alignStartIndex, alignedEdfVec] = tdtAlignEyeWithEdf(edfEyeVec, tdtEye
     edfFs = round(edfSamplingFreqHz);
     tdtFs = round(tdtSamplingFreqHz);
     tdtEyeVecResampled = single(resample(double(tdtEyeVec),edfFs,tdtFs));
-    alignStartIndex = alignVectors(edfEyeVec,tdtEyeVecResampled,alignWindowSecs * edfFs);
+    alignStartIndex = alignVectors(edfEyeVec,tdtEyeVecResampled,alignWindowSecs * edfFs, edfOptions);
     alignedEdfVec = edfEyeVec(alignStartIndex:end);
 end
 
-function [lag] = alignVectors(edfVec, tdtVec, slidingWinBins)
+function [lag] = alignVectors(edfVec, tdtVec, slidingWinBins, edfOptions)
     % for conversion to gaze in pixels
-    voltRange = [-5 5];
-    signalRange = [-0.2 1.2];
-    pixelRange = [0 1024]; % X-only
+    voltRange = edfOptions.voltRange; %[-5 5];
+    signalRange = edfOptions.signalRange; %[-0.2 1.2];
+    pixelRange = edfOptions.pixelRange; %[0 1024]; % X-only
     % In edf bin time 1ms if colledted at 1000Hz
-    %Restrict tdt eye data to double the slidingWinBins
-%     tdtNBins = numel(tdtVec);
-%     if tdtNBins > 2*slidingWinBins
-%         tdtNBins = 2*slidingWinBins;
-%         tdtVec = tdtVec(1:tdtNBins);
-%     end
-%     if numel(edfVec) > 2*slidingWinBins
-%         edfVec = edfVec(1:2*slidingWinBins);
-%     end
     tdtNBins = numel(tdtVec);
     nGazeEdf = (edfVec - min(edfVec))./range(edfVec);
     nGazeTdt = tdtAnalog2Pixels(tdtVec,voltRange,signalRange,pixelRange);
