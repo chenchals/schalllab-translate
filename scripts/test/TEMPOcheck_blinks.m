70a9ea969bd560f4fefeaf48eaf24c2dc15246d7
Chenchal Subraveti <chenchals@gmail.com>
2019-03-27 14:32:25 -0500

    updated blink explore...

--- scripts/test/TEMPOcheck_blinks.m
+++ scripts/test/TEMPOcheck_blinks.m
@@ -2,14 +2,14 @@
 baseDir = '/Volumes/schalllab';
 
 baseSaveDir = fullfile(baseDir,'Users/Chenchal/Tempo_NewCode/dataProcessed');
-sessName = 'Joule-190322-151707-Blinks';
+sessName = 'Joule-190326-154110-Blinks';
 sessionDir = fullfile(baseDir,'Users/Chenchal/Tempo_NewCode/Joule',sessName);
 
 
 load(fullfile(baseSaveDir,sessName, 'Events.mat'));
 load(fullfile(baseSaveDir,sessName, 'Eyes.mat'));
-if (exist(fullfile(sessionDir,'ProcLib/rawIVals.csv'),'file'))
-    tempoEyes = csvread(fullfile(sessionDir,'ProcLib/rawIVals.csv'));
+if (exist(fullfile(sessionDir,'ProcLib/rawIVals_1.csv'),'file'))
+    tempoEyes = csvread(fullfile(sessionDir,'ProcLib/rawIVals_1.csv'));
 end
 
 set(0, 'DefaultTextInterpreter', 'none')
@@ -18,10 +18,82 @@ set(0, 'DefaultTextInterpreter', 'none')
 Task = struct2table(Task);
 TaskInfos = struct2table(TaskInfos);
 if (exist('tempoEyes','var'))
-        tempoEyes = array2table(tempoEyes,'VariableNames',{'timeMs','eyeX','eyeY'});
+    %relTimeMs, EYE_X_VOLTS, EYE_Y_VOLTS, I_INVALID, I_INVALID_DURATION,pupil)
+        tempoEyes = array2table(tempoEyes,'VariableNames',{'timeMs','eyeX','eyeY','iInvalid','iInvalidDuration','pupil'});
         rowIds = tempoEyes.eyeX < -16000 & tempoEyes.eyeY < -16000;
         tempoEyes.noEyes(rowIds) = 1;
 end
+
+
+%% Plot eyes and pupil for whole data set
+
+x=tempoEyes.eyeX; x(x>-16000)=NaN;
+y=tempoEyes.eyeY; y(y>-16000)=NaN;
+p=tempoEyes.pupil; p(p>-10000)=NaN;
+noI=tempoEyes.noEyes; noI(noI==0)=NaN;
+dur=tempoEyes.iInvalidDuration; dur(dur==0)=NaN;
+invalid=tempoEyes.iInvalid; invalid(invalid==0)=NaN;
+
+loT=133460/2;
+hiT=133520/2;
+figure
+plot(tempoEyes.timeMs(loT:hiT),tempoEyes.eyeX(loT:hiT),'-b*');
+hold on
+plot(tempoEyes.timeMs(loT:hiT),tempoEyes.eyeY(loT:hiT),'-r*');
+hold on
+plot(tempoEyes.timeMs(loT:hiT),tempoEyes.pupil(loT:hiT),'-g*');
+hold on
+plot(tempoEyes.timeMs(loT:hiT),tempoEyes.iInvalid(loT:hiT)*-16000,'-k*');
+hold on
+%plot(tempoEyes.timeMs,dur,'-m');
+hold off
+grid on
+
+x=tempoEyes.eyeX; x(x>-16000)=NaN;
+y=tempoEyes.eyeY; y(y>-16000)=NaN;
+p=tempoEyes.pupil; p(p>-10000)=NaN;
+noI=tempoEyes.noEyes; noI(noI==0)=NaN;
+dur=tempoEyes.iInvalidDuration; dur(dur==0)=NaN;
+invalid=tempoEyes.iInvalid; invalid(invalid==0)=NaN;
+
+figure
+plot(tempoEyes.timeMs,x,'-b');
+hold on
+plot(tempoEyes.timeMs,y,'-r');
+hold on
+plot(tempoEyes.timeMs,p,'-g');
+hold on
+plot(tempoEyes.timeMs,noI,'-k');
+hold on
+plot(tempoEyes.timeMs,dur,'-m');
+hold off
+
+
+
+d=diff(tempoEyes.iInvalidDuration);
+
+
+idxNaN = tempoEyes.eyeX<12000 & tempoEyes.eyeX>-12000;
+x=tempoEyes.eyeX; x(~idxNaN)=NaN;
+y=tempoEyes.eyeY; y(~idxNaN)=NaN;
+p=tempoEyes.pupil; p(~idxNaN)=NaN;
+noI=tempoEyes.noEyes; noI(~idxNaN)=NaN;
+dur=tempoEyes.iInvalidDuration; dur(~idxNaN)=NaN;
+invalid=tempoEyes.iInvalid; invalid(~idxNaN)=NaN;
+
+
+figure
+plot(tempoEyes.timeMs,x,'-b');
+hold on
+plot(tempoEyes.timeMs,y,'-r');
+hold on
+plot(tempoEyes.timeMs,p,'-g');
+hold on
+plot(tempoEyes.timeMs,invalid*-16000,'-k');
+hold on
+
+
+
 
 % count ones in diff and group: see accumarray
 A = tempoEyes.noEyes;
