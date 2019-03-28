11e4d15997a517b414cef842a6c8a4a88120e5e1
Chenchal Subraveti <chenchals@gmail.com>
2019-03-28 16:59:08 -0500

    updated inh fx

--- scripts/cmdOnline/onlineBehPlots.m
+++ scripts/cmdOnline/onlineBehPlots.m
@@ -2,9 +2,9 @@
 
 figure; 
 yyaxis('left');
-plot(beh.inhibition.ssdStatsAll.mean_UseSsdIdx+1, beh.inhibition.values.pNC,'o-b','LineWidth',2,'MarkerSize',10);
+plot(beh.inhibition.ssdStatsAll.mean_UseSsdIdx+1, beh.inhibition.values.pNC,'o-b','LineWidth',2,'MarkerSize',14);
 hold on
-plot(beh.inhibition.ssdStatsCancelled.mean_UseSsdIdx+1, beh.inhibition.values.pNC(1:3),'+','MarkerSize', 10);
+plot(beh.inhibition.ssdStatsCancelled.mean_UseSsdIdx+1, beh.inhibition.values.pNC(1:3),'d','MarkerSize', 8, 'MarkerFaceColor','k','MarkerEdgeColor','y');
 hold on
 ylim([0 1.1])
 ylabel('pNC')
@@ -13,7 +13,7 @@ grid on
 title('Inhibition function')
 yyaxis('right');
 bb = bar(beh.inhibition.ssdStatsAll.mean_UseSsdIdx+1,beh.inhibition.values.nTrials,...
-    'BarWidth',0.95);
+    'BarWidth',0.95,'FaceAlpha',0.6);
 ylim([0 max(beh.inhibition.values.nTrials)*1.1])
 xticklabels([0; beh.inhibition.ssdStatsAll.mean_UseSsdVrCount])
 set(gca, 'SortMethod', 'depth')
