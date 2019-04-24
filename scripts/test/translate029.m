e6ed00ec66a1881a21e596acfb7647c92cf5fcc1
Chenchal Subraveti <chenchals@gmail.com>
2019-04-24 09:14:45 -0500

    updated all translations

--- scripts/test/translate029.m
+++ scripts/test/translate029.m
@@ -3,7 +3,7 @@
 baseDir = '/Volumes/schalllab';
 sessionBaseDir = fullfile(baseDir,'Users/Chenchal/Tempo_NewCode/Joule');
 baseSaveDir = fullfile(baseDir,'Users/Chenchal/Tempo_NewCode/dataProcessed');
-sessName = 'Joule-190416-103550';
+sessName = 'Joule-190423-110826';
 procLibDir =fullfile(sessionBaseDir, sessName, 'ProcLib');
 eventDefFile = fullfile(procLibDir,'CMD/EVENTDEF.PRO');
 infosDefFile = fullfile(procLibDir,'CMD/INFOS.PRO');
