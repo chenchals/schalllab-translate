045503a3713bc6b767fb38e5e7b25400cf0d6719
Chenchal Subraveti <chenchals@gmail.com>
2019-06-28 14:27:25 -0500

    Housekeeping....

--- scratch/translateEphys.m
+++
@@ -1,7 +0,0 @@
-
-dataDir = '/Users/subravcr/teba/data/Joule/cmanding/ephys/TESTDATA';
-session = 'Joule-190510-111052'; % No RAW data
-
-blockPath=fullfile(dataDir,session);
-
-H=TDTbin2mat(blockPath,'headers',1)
