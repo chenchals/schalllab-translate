a3d902623535887e5aa03cbc02da970442ea5452
Chenchal Subraveti <chenchals@gmail.com>
2019-07-10 08:29:37 -0500

    Added JRCLUST .prb probe files

--- scratch/tdtKs2/tdt2KsBin.m
+++ scratch/tdtKs2/tdt2KsBin.m
@@ -1,6 +1,6 @@
 % convert tdt _Wav1_ or _RSn1_ .sev files into a kssort type binary
 % interleaved data format
-sess=dir('/scratch/ksData/Joule/cmanding/ephys/TESTDATA/Joule*');
+sess=dir('/scratch/ksData/Rig029/Joule*');
 
 for f = 1:size(sess)
     ds = fullfile(sess(f).folder,sess(f).name,'*_Wav1_*.sev');
