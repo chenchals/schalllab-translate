df711c25c24a1c767c07449603974843926765f1
Chenchal Subraveti <chenchals@gmail.com>
2019-07-19 07:48:44 -0500

    updated for KiloSort (1) cofiguration

---
+++ scratch/ks1/ks1_master_file.m
@@ -0,0 +1,30 @@
+% default options are in parenthesis after the comment
+
+addpath(genpath('/Users/subravcr/Projects/SpikeSorting/KiloSort')) % path to kilosort folder
+addpath(genpath('/Users/subravcr/Projects/SpikeSorting/npy-matlab')) % path to npy-matlab scripts
+
+pathToYourConfigFile = '/Users/subravcr/Projects/lab-schall/schalllab-translate/scratch/ks1'; % take from Github folder and put it somewhere else (together with the master_file)
+run(fullfile(pathToYourConfigFile, 'ks1_tdt_config.m'))
+
+tic; % start timer
+%
+if ops.GPU     
+    gpuDevice(1); % initialize GPU (will erase any existing GPU arrays)
+end
+%
+[rez, DATA, uproj] = preprocessData(ops); % preprocess data and extract spikes for initialization
+rez                = fitTemplates(rez, DATA, uproj);  % fit templates iteratively
+rez                = fullMPMU(rez, DATA);% extract final spike times (overlapping extraction)
+
+% AutoMerge. rez2Phy will use for clusters the new 5th column of st3 if you run this)
+%     rez = merge_posthoc2(rez);
+
+% save matlab results file
+save(fullfile(ops.root,  'rez.mat'), 'rez', '-v7.3');
+
+% save python results file for Phy
+rezToPhy(rez, ops.root);
+
+% remove temporary file
+delete(ops.fproc);
+%%
