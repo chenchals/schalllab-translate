c639f6f238ecd92b1906dffad573fcddc38c3f87
Chenchal Subraveti <chenchals@gmail.com>
2019-07-31 16:32:27 -0500

    updated to process on Kaleb's machine

--- scratch/spikeSort/master_ks2.m
+++ scratch/spikeSort/master_ks2.m
@@ -1,9 +1,9 @@
 
-dataPath     = '/mnt/teba/data/Joule/cmanding/ephys/TESTDATA/In-Situ';
-analysisDir = '/scratch/subravcr/ksDataProcessed/Joule/cmanding/ephys/TESTDATA/In-Situ';
-session     = 'Joule-190725-111052';%'Joule-190725-111500';
-chanMapFile = '/home/subravcr/Projects/lab-schall/schalllab-translate/toolbox/spk-cluster/channelMaps/linear-probes-1-4-chan-150um.mat';
-nChan = 4;
+dataPath     = '/home/subravcr/teba/data/Darwin/proNoElongationColor_physio';
+analysisDir = '/home/subravcr/scratch/subravcr/ksDataProcessed/Darwin/proNoElongationColor_physio';
+session     = 'Darwin-190729-112447';%'Joule-190725-111052'; %'Joule-190725-111500';
+chanMapFile = '/home/subravcr/Projects/lab-schall/schalllab-translate/toolbox/spk-cluster/channelMaps/linear-probes-1-32-chan-150mu.mat';
+nChan = 32;
 sessionAnalysisDir = fullfile(analysisDir,session);
 
 %% Params and configuration for Kilosort2
@@ -19,7 +19,7 @@ ops.root                = sessionAnalysisDir;
 ops.fbinary             = fullfile(ops.root, [session '.bin']); % will be created for 'openEphys'
 rootZ                   = fullfile(ops.root,'ks2');
 ops.fproc               = fullfile(rootZ, 'temp_wh.dat'); % residual from RAM of preprocessed data
-ops.trange              = [0 100];	% time range to sort
+ops.trange              = [0 Inf];	% time range to sort
 % need ops.nt0 for fitTemplates
 ops.nt0                 = 61; % length of samples for waveform data?
 
@@ -34,9 +34,9 @@ end
 ops.fs                  = 24414;        % sampling rate
 ops.NchanTOT            = nChan;           % total number of channels
 ops.Nchan               = nChan;           % number of active channels 
-ops.Nfilt               = 128;           % number of filters to use (512, should be a multiple of 32)     
-ops.nNeighPC            = []; % visualization only (Phy): number of channnels to mask the PCs, leave empty to skip (12)
-ops.nNeigh              = []; % visualization only (Phy): number of neighboring templates to retain projections of (16)
+ops.Nfilt               = 512;           % number of filters to use (512, should be a multiple of 32)     
+ops.nNeighPC            = [12]; % visualization only (Phy): number of channnels to mask the PCs, leave empty to skip (12)
+ops.nNeigh              = [16]; % visualization only (Phy): number of neighboring templates to retain projections of (16)
 %% Channel map file
 % define the channel map as a filename (string) or simply an array
 [~,fn]=fileparts(chanMapFile);
@@ -45,16 +45,16 @@ copyfile(chanMapFile, dest,'f');
 ops.chanMap             = dest; % make this file using createChannelMapFile.m
 
 % frequency for high pass filtering (150)
-ops.fshigh = 150;   
+ops.fshigh = 300;   
 
 % minimum firing rate on a "good" channel (0 to skip)
 ops.minfr_goodchannels  = 0.1; 
 
 % threshold on projections (like in Kilosort1, can be different for last pass like [10 4])
-ops.Th                  = [10 4];  
+ops.Th                  = [6 12 12];  
 
 % how important is the amplitude penalty (like in Kilosort1, 0 means not used, 10 is average, 50 is a lot) 
-ops.lam                 = 10;  
+ops.lam                 = [10 30 30];  
 
 % splitting a cluster at the end requires at least this much isolation for each sub-cluster (max = 1)
 ops.AUCsplit            = 0.9; 
@@ -69,20 +69,20 @@ ops.momentum            = [20 400];
 ops.sigmaMask           = 30; 
 
 % threshold crossings for pre-clustering (in PCA projection space)
-ops.ThPre               = 8; 
+ops.ThPre               = 6; 
 %% danger, changing these settings can lead to fatal errors
 % options for determining PCs
-ops.spkTh               = -4.5;      % spike threshold in standard deviations (-6)
+ops.spkTh               = -4;      % spike threshold in standard deviations (-6)
 ops.reorder             = 1;       % whether to reorder batches for drift correction. 
-ops.nskip               = 25;  % how many batches to skip for determining spike PCs
+ops.nskip               = 1;  % how many batches to skip for determining spike PCs
 % ks1: ops.Nfilt               = 960;  % number of filters to use (512, should be a multiple of 32)
 ops.Nfilt               = 1024; % max number of clusters
-ops.nfilt_factor        = 4; % max number of clusters per good channel (even temporary ones)
+ops.nfilt_factor        = 10; % max number of clusters per good channel (even temporary ones)
 ops.ntbuff              = 64;    % samples of symmetrical buffer for whitening and spike detection
-ops.NT                  = 64*1024+ ops.ntbuff; % must be multiple of 32 + ntbuff. This is the batch size (try decreasing if out of memory). 
+ops.NT                  = 32*64*1024+ ops.ntbuff; % must be multiple of 32 + ntbuff. This is the batch size (try decreasing if out of memory). 
 ops.whiteningRange      = 32; % number of channels to use for whitening each channel
-ops.nSkipCov            = 25; % compute whitening matrix from every N-th batch
-ops.scaleproc           = 200;   % int16 scaling of whitened data
+ops.nSkipCov            = 1; % compute whitening matrix from every N-th batch
+ops.scaleproc           = 1;   % int16 scaling of whitened data
 ops.nPCs                = 3; % how many PCs to project the spikes into
 ops.useRAM              = 0; % not yet available
 
