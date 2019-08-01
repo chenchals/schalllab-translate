285bb73d3c6ca644fa6792e0891ad3ed7f281a9e
Chenchal Subraveti <chenchals@gmail.com>
2019-08-01 09:21:42 -0500

    updated to process on Kaleb's machine

--- scratch/spikeSort/exploreWaveforms.m
+++ scratch/spikeSort/exploreWaveforms.m
@@ -1,9 +1,46 @@
 
 
-ks1Phy = '/scratch/subravcr/ksDataProcessed/Joule/cmanding/ephys/TESTDATA/In-Situ/Joule-190725-111052/ks1_0';
-npyFiles = dir([ks1Phy '/*.npy']);
+%ks1Phy = '/scratch/subravcr/ksDataProcessed/Joule/cmanding/ephys/TESTDATA/In-Situ/Joule-190725-111052/ks1_0';
+%npyFiles = dir([ks1Phy '/*.npy']);
+baseDataLoc = 'data/Joule/cmanding/ephys/TESTDATA/In-Situ';
+tank = 'Joule-190731-121704';
+wavBase = fullfile(baseDataLoc, tank);
+% see if you can get waveforms from the Box thresholds
+% tdtAllsData = TDTbin2mat(fullfile(baseDataLoc,tank));
+% get only BOx1 data that is stored: store name : eBo1
+tdtEbo1 = TDTbin2mat(fullfile(baseDataLoc,tank),'STORE',{'eBo1'});
+tdtInfo = tdtEbo1.info;
+tdtEbo1 = tdtEbo1.snips.eBo1;
 
-wavFn='/scratch/subravcr/ksData/Joule/cmanding/ephys/TESTDATA/In-Situ/Joule-190725-111500/CMD_TSK_029_EphysSingle-190724-081723_Joule-190725-111500_Wav1_Ch';
+% for each channel get indexof channel & sortcode
+uniqSortcodes = unique(tdtEbo1.sortcode);
+uniqChanNums = unique(tdtEbo1.chan);
+for c = 1:numel(uniqChanNums)
+    chans.sortIdx{c} = arrayfun(@(x) ...
+           find(tdtEbo1.chan == uniqChanNums(c) & tdtEbo1.sortcode == x),...
+           uniqSortcodes,'UniformOutput',false);
+    % chanTimestamps
+    chans.ts{c} = arrayfun(@(x) ...
+           tdtEbo1.ts(tdtEbo1.chan == uniqChanNums(c) & tdtEbo1.sortcode == x),...
+           uniqSortcodes,'UniformOutput',false);
+     % chanWavforms
+    chans.wf{c} = arrayfun(@(x) ...
+           tdtEbo1.data(tdtEbo1.chan == uniqChanNums(c) & tdtEbo1.sortcode == x, :),...
+           uniqSortcodes,'UniformOutput',false);
+end
+
+plot(chans.wf{1}{2}(1:10,:))
+
+
+
+
+% Check the recording quality
+wavFns = dir([wavBase '/*Wav1_Ch*.sev']);
+sampleType = 'single';
+sampleWidth = 4;
+dataShape = [size(wavFns,1), (wavFns(1).bytes-40)/sampleWidth];
+wavFns = strcat({wavFns.folder},filesep,{wavFns.name})';
+
 
 wavCh1=memmapfile([wavFn '1.sev'],'Offset', 40, 'Format','single');
 wavCh2=memmapfile([wavFn '2.sev'],'Offset', 40, 'Format','single');
